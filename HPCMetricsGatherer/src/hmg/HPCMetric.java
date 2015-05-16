abbdd56ad97d296c2a8c363849a5e6cd2cc66d68
Teedles <andrew@dev.fritz.box>
2015-05-16 03:02:17 +0200

    session compute time ok-ish now.

--- HPCMetricsGatherer/src/hmg/HPCMetric.java
+++ HPCMetricsGatherer/src/hmg/HPCMetric.java
@@ -13,12 +13,10 @@ import com.datasynapse.gridserver.GridServerException;
 import com.datasynapse.gridserver.admin.AdminManager;
 import com.datasynapse.gridserver.admin.BrokerAdmin;
 import com.datasynapse.gridserver.admin.BrokerInfo;
-import com.datasynapse.gridserver.admin.EngineAdmin;
 import com.datasynapse.gridserver.admin.EngineInfo;
-import com.datasynapse.gridserver.admin.ServiceAdmin;
 import com.datasynapse.gridserver.admin.ServiceInfo;
+import com.datasynapse.gridserver.admin.ServiceInvocationInfo;
 import com.datasynapse.gridserver.driver.DriverManager;
-
 
 public class HPCMetric implements Runnable {
 
@@ -27,16 +25,9 @@ public class HPCMetric implements Runnable {
 	private Calendar currenttime;
 	protected Metric m;
 	protected DriverGegevens d;
-	protected List<Metric> brokerMetricTotalList;
-	protected List<Metric> brokerMetricBusyList;
-	protected List<Metric> serviceMetricUList;
-	protected List<Metric> serviceMetricSList;
-	protected List<Metric> serviceMetricEList;
-	protected List<Metric> serviceMetricCList;
-	protected List<Metric> engineMetricList;
 	protected List<Metric> finalMetricList;
 
-	List<List<List<Metric>>> arrayListOflistsOfMetricsLists = new ArrayList<List<List<Metric>>>(); 
+	List<List<List<Metric>>> arrayListOflistsOfMetricsLists; 
 
 	public HPCMetric() {
 
@@ -88,14 +79,6 @@ public class HPCMetric implements Runnable {
 		DriverManager.fillDefaults();
 		DriverManager.addProperties(p);	
 
-		brokerMetricTotalList = new ArrayList<Metric>();
-		brokerMetricBusyList = new ArrayList<Metric>();
-		serviceMetricUList = new ArrayList<Metric>();
-		serviceMetricSList = new ArrayList<Metric>();
-		serviceMetricEList = new ArrayList<Metric>();
-		serviceMetricCList = new ArrayList<Metric>();
-		engineMetricList = new ArrayList<Metric>();
-
 		try {
 			DriverManager.connect();
 		} catch (GridServerException g) {
@@ -108,19 +91,15 @@ public class HPCMetric implements Runnable {
 
 			if (DriverManager.isConnected()) {
 
+				arrayListOflistsOfMetricsLists = new ArrayList<List<List<Metric>>>();
 				//
 				//  Metrics
 				//
 
 				// Datasynapse Classes
 				BrokerAdmin ba = AdminManager.getBrokerAdmin();
-				ServiceAdmin sa = AdminManager.getServiceAdmin();
-				ServiceInfo[] si = sa.getAllServiceInfo();	
-				EngineAdmin ea = AdminManager.getEngineAdmin();
-				EngineInfo[] ei = ea.getAllEngineInfo();
-
 				BrokerInfo[] bi = ba.getAllBrokerInfo();
-				int brokerNumber = bi.length;
+				int brokerNumber = ba.getBrokerCount();
 
 				for (int bn = 0; bn < brokerNumber; bn++) {
 
@@ -145,29 +124,35 @@ public class HPCMetric implements Runnable {
 						}
 					}
 
-					int pendingTasks =0;
-					int runningTasks =0;
-					int completedTasks =0;
-					int serviceError =0;
-
-					//DriverManager.connect(bi[bn].getName());
-
 					String[] bh = bi[bn].getBaseUrl().split("//");
 					String[] bh2 = bh[1].split(":");
 					String[] bh3 = bh2[0].split("\\.");
 					String bhfinal = bh3[0];
 
-					//if (DriverManager.isConnected()) {
+					DriverManager.connect(bi[bn].getName());
+
 					try {						
 						// Housekeeping lists Engine and Session data
 						List<ServiceData> serviceDataList = new ArrayList<ServiceData>();
 						List<EngineData> engineDataList = new ArrayList<EngineData>();
+						int pendingTasks =0;
+						int runningTasks =0;
+						int completedTasks =0;
+						int serviceError =0;
 
-						// Engine Metrics
+						//
+						// Broker Metrics
+						//
 						arrayListOflistsOfMetricsLists.get(bn).get(0).add(fillMetric(bhfinal, "TotalEngines", bi[bn].getEngineCount()));
 						arrayListOflistsOfMetricsLists.get(bn).get(1).add(fillMetric(bhfinal, "BusyEngines", bi[bn].getBusyEngineCount()));
 
+						//
+						// Service Metrics
+						//
+						ServiceInfo[] si = AdminManager.getServiceAdmin().getAllServiceInfo();	
+
 						if (si != null) {
+
 							for (ServiceInfo s : si) {
 
 								if (! s.getFinished() &&  ! s.getCompleted()) {
@@ -192,13 +177,23 @@ public class HPCMetric implements Runnable {
 									completedTasks += s.getCompletedCount();
 								}
 							}
-
-							// Service Metrics
-							arrayListOflistsOfMetricsLists.get(bn).get(2).add(fillMetric(bhfinal, "U", runningTasks));
-							arrayListOflistsOfMetricsLists.get(bn).get(3).add(fillMetric(bhfinal, "S", pendingTasks));
-							arrayListOflistsOfMetricsLists.get(bn).get(4).add(fillMetric(bhfinal, "E", serviceError));
-							arrayListOflistsOfMetricsLists.get(bn).get(5).add(fillMetric(bhfinal, "C", completedTasks));
 						}
+
+						arrayListOflistsOfMetricsLists.get(bn).get(2).add(fillMetric(bhfinal, "U", runningTasks));
+						arrayListOflistsOfMetricsLists.get(bn).get(3).add(fillMetric(bhfinal, "S", pendingTasks));
+						arrayListOflistsOfMetricsLists.get(bn).get(4).add(fillMetric(bhfinal, "E", serviceError));
+						arrayListOflistsOfMetricsLists.get(bn).get(5).add(fillMetric(bhfinal, "C", completedTasks));
+
+						//reset counters
+						pendingTasks = 0;
+						runningTasks = 0;
+						completedTasks = 0;
+						serviceError = 0;
+
+						//
+						// Engine Metrics
+						//						
+						EngineInfo[] ei = AdminManager.getEngineAdmin().getAllEngineInfo();
 
 						if (ei != null) {
 							for (EngineInfo i : ei) {
@@ -213,6 +208,7 @@ public class HPCMetric implements Runnable {
 									for (ServiceData s : serviceDataList) {
 										if (s.getSessionID() == e.getServiceID()) {
 											e.setSessionName(s.getSessionName().replaceAll("\\s+", ""));
+
 										}
 										engineDataList.add(e);
 									}
@@ -220,28 +216,24 @@ public class HPCMetric implements Runnable {
 							}
 
 							// get total compute time per session
-
 							if (serviceDataList.size() != 0) {
 								for (ServiceData s : serviceDataList) {
 
+									ServiceInvocationInfo[] sii = AdminManager.getServiceAdmin().getSelectedInvocationInfo(s.getSessionID(), null);
 									int totalComputeTime = 0;								
 									String summedSessionName = s.getSessionName().replaceAll("\\s+", "");	
 
-									for (EngineData e : engineDataList) {
-
-										if (summedSessionName.equalsIgnoreCase(e.getSessionName())) {
-											totalComputeTime +=  e.getComputeTime();
-										}
+									for (int i = 0; i < sii.length; i++) {
+										totalComputeTime += sii[i].getElapsedTime();
 									}
 
-									arrayListOflistsOfMetricsLists.get(bn).get(6).add(fillMetric(bhfinal + ".sessions." + summedSessionName, "ComputeTime" , totalComputeTime));
+									arrayListOflistsOfMetricsLists.get(bn).get(6).add(fillMetric(bhfinal + ".sessions." + summedSessionName, "ComputeTime" , (totalComputeTime / 1000)));
 								}
 							}
 						}
 					} catch ( NullPointerException e) {
 						e.getStackTrace();
 					}
-					//}
 				}
 			}
 
@@ -249,19 +241,16 @@ public class HPCMetric implements Runnable {
 
 				finalMetricList = new ArrayList<Metric>();
 
+				// for every broker
 				for (int i = 0; i < arrayListOflistsOfMetricsLists.size(); i++) {
 
 					int finalBrokerMaxTotal = 0;
 					int finalBrokerMaxBusy = 0;
-					int finalSessionMaxU = 0;
-					int finalSessionMaxS = 0;
-					int finalSessionMaxE = 0;
-					int finalSessionMaxC = 0;
-					int finalEngineComputeTime = 0;
+
 					String sessionName = "NotAName!";
 
 					//
-					// Sum all Broker Metrics
+					// MAX Broker Metrics
 					//
 
 					finalBrokerMaxTotal = getMaxWaarde(arrayListOflistsOfMetricsLists.get(i).get(0));
@@ -272,30 +261,37 @@ public class HPCMetric implements Runnable {
 
 					List<List<Metric>> tempList = null;
 
-					if (arrayListOflistsOfMetricsLists.get(i).get(2).size() > 0 ) {
+					int finalSessionMaxU = 0;
+					int finalSessionMaxS = 0;
+					int finalSessionMaxE = 0;
+					int finalSessionMaxC = 0;
+
+					if (arrayListOflistsOfMetricsLists.get(i).get(2).size() != 0 ) {
 
 						finalSessionMaxU = getMaxWaarde(arrayListOflistsOfMetricsLists.get(i).get(2));
 						finalSessionMaxS = getMaxWaarde(arrayListOflistsOfMetricsLists.get(i).get(3));
 						finalSessionMaxE = getMaxWaarde(arrayListOflistsOfMetricsLists.get(i).get(4));
 						finalSessionMaxC = getMaxWaarde(arrayListOflistsOfMetricsLists.get(i).get(5));
+					}
 
-						finalMetricList.add(fillMetric(arrayListOflistsOfMetricsLists.get(i).get(2).get(0).getBron(), arrayListOflistsOfMetricsLists.get(i).get(2).get(0).getType(), finalSessionMaxU));
-						finalMetricList.add(fillMetric(arrayListOflistsOfMetricsLists.get(i).get(3).get(0).getBron(), arrayListOflistsOfMetricsLists.get(i).get(3).get(0).getType(), finalSessionMaxS));
-						finalMetricList.add(fillMetric(arrayListOflistsOfMetricsLists.get(i).get(4).get(0).getBron(), arrayListOflistsOfMetricsLists.get(i).get(4).get(0).getType(), finalSessionMaxE));
-						finalMetricList.add(fillMetric(arrayListOflistsOfMetricsLists.get(i).get(5).get(0).getBron(), arrayListOflistsOfMetricsLists.get(i).get(5).get(0).getType(), finalSessionMaxC));
-
+					if (arrayListOflistsOfMetricsLists.get(i).get(6).size() != 0 ) {
 						// magic stuff :-)
 						Collections.sort(arrayListOflistsOfMetricsLists.get(i).get(6));
+
 						tempList = new ArrayList<List<Metric>>(); 
+
 						int listCounter = -1;
 
 						for (Metric m : arrayListOflistsOfMetricsLists.get(i).get(6)) {
 
 							if (!sessionName.equalsIgnoreCase(m.getType())) {
+
 								tempList.add( new ArrayList<Metric>());
+
 								listCounter++;
+
 								tempList.get(listCounter).add(m);
-								
+
 								sessionName = m.getType();
 							} else {
 								tempList.get(listCounter).add(m);
@@ -303,12 +299,18 @@ public class HPCMetric implements Runnable {
 						}
 					}
 
-					for (int tls = 0; tls < tempList.size(); tls++) {
+					finalMetricList.add(fillMetric(arrayListOflistsOfMetricsLists.get(i).get(2).get(0).getBron(), arrayListOflistsOfMetricsLists.get(i).get(2).get(0).getType(), finalSessionMaxU));
+					finalMetricList.add(fillMetric(arrayListOflistsOfMetricsLists.get(i).get(3).get(0).getBron(), arrayListOflistsOfMetricsLists.get(i).get(3).get(0).getType(), finalSessionMaxS));
+					finalMetricList.add(fillMetric(arrayListOflistsOfMetricsLists.get(i).get(4).get(0).getBron(), arrayListOflistsOfMetricsLists.get(i).get(4).get(0).getType(), finalSessionMaxE));
+					finalMetricList.add(fillMetric(arrayListOflistsOfMetricsLists.get(i).get(5).get(0).getBron(), arrayListOflistsOfMetricsLists.get(i).get(5).get(0).getType(), finalSessionMaxC));
 
-						finalEngineComputeTime = getMaxWaarde(tempList.get(tls)); 
-						finalMetricList.add(fillMetric(arrayListOflistsOfMetricsLists.get(i).get(6).get(0).getBron(), arrayListOflistsOfMetricsLists.get(i).get(6).get(0).getType(), finalEngineComputeTime));
+					if (tempList != null) {
+						int finalEngineComputeTime = 0;
+						for (int tls = 0; tls < tempList.size(); tls++) {
+							finalEngineComputeTime = getSumWaarde(tempList.get(tls)); 
+							finalMetricList.add(fillMetric(arrayListOflistsOfMetricsLists.get(i).get(6).get(0).getBron(), arrayListOflistsOfMetricsLists.get(i).get(6).get(0).getType(), finalEngineComputeTime));
+						}
 					}
-
 				}	
 
 				// all final metrics go in to the queue
@@ -317,19 +319,10 @@ public class HPCMetric implements Runnable {
 					queue.add(m);
 				}
 
-				// reset the counter
+				// reset the counter and list of lists
 				counter = 0;
-
-				// reset the lists
-				brokerMetricTotalList = new ArrayList<Metric>();
-				brokerMetricBusyList = new ArrayList<Metric>();
-				serviceMetricUList = new ArrayList<Metric>();
-				serviceMetricSList = new ArrayList<Metric>();
-				serviceMetricEList = new ArrayList<Metric>();
-				serviceMetricCList = new ArrayList<Metric>();
-				engineMetricList = new ArrayList<Metric>();
-
-
+				arrayListOflistsOfMetricsLists = new ArrayList<List<List<Metric>>>();
+	
 			} else {
 				counter++;
 			}
@@ -364,6 +357,18 @@ public class HPCMetric implements Runnable {
 			return maxWaarde;
 		}
 		return 0;
+	}
+
+	private int getSumWaarde(List<Metric> list) {
+
+		int sumWaarde = 0;
+
+		for (Metric m : list) {
+
+			sumWaarde += m.getWaarde();
+		}
+
+		return sumWaarde;
 	}
 
 }
--- HPCMetricsGatherer/src/hmg/ServiceData.java
+++ HPCMetricsGatherer/src/hmg/ServiceData.java
@@ -3,6 +3,7 @@ package hmg;
 public class ServiceData {
 
 	private long SessionID;
+	private long[] invocationID;
 	private String SessionName;
 	
 	public ServiceData() {
@@ -23,6 +24,25 @@ public class ServiceData {
 
 	public void setSessionName(String sessionName) {
 		SessionName = sessionName;
+	}
+
+	public long[] getInvocationID() {
+		return invocationID;
+	}
+
+	public void setInvocationID(long[] invocationID) {
+		this.invocationID = invocationID;
+	}
+	
+	public void addInvocationID(long invocationID) {
+		
+		int l = this.invocationID.length;
+		
+		if (l == 0) {
+			this.invocationID[l] = invocationID;
+		} else {
+			this.invocationID[l + 1] = invocationID;
+		}
 	}
 
 }
